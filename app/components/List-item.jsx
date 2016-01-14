8375ec38cb5ed2e3c859bd6222981e25dcf71cdf
kweng2 <kweng2@gmail.com>
2016-01-13 21:30:00 -0800

    dropdown menu works now

--- app/components/List-item.jsx
+++ app/components/List-item.jsx
@@ -1,12 +1,8 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  handleClick: function() {
-    this.props.whenItemClicked(this.props.item);
-  },
+
   render: function () {
-    return <li className={this.props.className}>
-    <a onClick={this.handleClick}>{this.props.item}</a>
-    </li>
+    return <option value={this.props.item}>{this.props.item}</option>
   }
 });
--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -23,8 +23,9 @@ var Map = React.createClass({
   handleCommentChange(e) {
     this.setState({comment: e.target.value});
   },
+
   handleCategoryChange(e) {
-    this.setState({category: e.target.title});
+    this.setState({category: e.target.value});
   },
 
 
@@ -268,7 +269,7 @@ var Map = React.createClass({
       </div>
       <form  onSubmit={this.handleSubmit} className="form-group list-group col-xs-12 col-md-6 col-md-offset-3" >
         <label htmlFor="category">Category:</label>
-        <DropDown id='category' title='General' items={['Food', 'Nature', 'Pets', 'Sports', 'Music', 'General']} onChange={this.handelCategoryChange} />
+        <DropDown id='category' title='General' items={['Food', 'Nature', 'Pets', 'Sports', 'Music', 'General']} onChange={this.handleCategoryChange} />
         <label htmlFor="location">Location:</label>
         <input type="text" className="form-control" id="location" onChange={this.handleLocationChange} value={this.state.location} placeholder="Location" />
         <label htmlFor="comment">Comment:</label>
