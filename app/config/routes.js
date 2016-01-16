1686d87e39042919af4ffa9d5ea9c6fd19b2017c
Kris Albert Lee <krisalbert@gmail.com>
2016-01-15 19:25:40 -0800

    Implement Facebook login

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,7 +2,6 @@ var React = require('react');
 var Main = require('../components/Main.jsx');
 var Home = require('../components/Home.jsx');
 var Login = require('../components/Login.jsx');
-var Signup = require('../components/Signup.jsx');
 var Router = require('react-router');
 var Route = Router.Route;
 var IndexRoute = Router.IndexRoute;
@@ -14,8 +13,6 @@ module.exports = (
   <Route path="/" component={Main}>
     <IndexRoute component={MapApp} />
     <Route name="login" path="login" component={Login} />
-    <Route name="home" path="home" component={Home} />
     <Route name="map" path="map" component={MapApp} />
-    <Route name="signup" path="signup" component={Signup} />
   </Route>
 );
