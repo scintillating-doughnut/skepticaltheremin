8c92b78b5dbf0841496d0c0c755f5c8dca26eb9a
kweng2 <kweng2@gmail.com>
2016-01-15 20:08:46 -0800

    ready for deployment

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-var router = require('./router/router.js')
+var router = require('./router/router.js');
 var userController = require('./controllers/userControllers.js');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 // var MONGO_DB = "mongodb://mewpeter:Peirce234@ds037415.mongolab.com:37415/breadcrumbs";
-var MONGO_DB;
-mongoose.connect(MONGO_DB || 'mongodb://localhost/maps');
+var mongodbURI = process.env.MONGOLAB_URI || 'mongodb://localhost/maps';
+
+mongoose.connect(mongodbURI);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -24,13 +25,13 @@ app.use(express.static(path.join(__dirname, "/public")));
   // need to add middleware for actual loging authentication
 app.get('/signup', function(req, res){
   console.log('signup');
-  res.redirect('/#map')
+  res.redirect('/#map');
 });
 
 app.post('/login', function(req, res){
   console.log('signup');
-  res.redirect('/#map')
-})
+  res.redirect('/#map');
+});
 
 
 //api routing
@@ -40,6 +41,6 @@ app.use('/api', router);
 
 
 console.log('mapp app on port ' + port);
-app.listen(port);  
+app.listen(port);
 
 module.exports = app;
